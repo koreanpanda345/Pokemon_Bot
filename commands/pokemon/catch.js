4f955786cbc2c92d5a93db77ff27083e134d74d1
Nathaniel <64607128+Natemo6348@users.noreply.github.com>
2020-12-04 08:11:43 -0600

    Update catch.js

--- commands/pokemon/catch.js
+++ commands/pokemon/catch.js
@@ -1,5 +1,5 @@
 const {Command} = require('discord.js-commando');
-const{RichEmbed} = require('discord.js');
+const{MessageEmbed} = require('discord.js');
 const db = require('quick.db');
 const Pokedex = require('pokedex-promise-v2');
 const P = new Pokedex();
@@ -34,7 +34,7 @@ module.exports = class CatchCommand extends Command{
             let shiny = 0;
             db.add(`pokemon_amount_${message.author.id}.num`, 1);
             db.set(`pokemon_${db.get(`pokemon_amount_${message.author.id}.num`)}_${message.author.id}`, {name: guess.toLowerCase(), level:1, exp: 0,hp:hp, atk: atk, def: def, spatk: spatk, spdef: spdef, speed: speed, shiny: shiny});
-            let embed = new RichEmbed()
+            let embed = new MessageEmbed()
             .setTitle(`Congratulations ${message.author.username}'s you caught a **level ${level} ${guess.toLowerCase()}**`);
 
             message.channel.send(embed);
